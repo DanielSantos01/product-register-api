9f57aac3b06d964eb455cfb79763e034f6c4a31d
Daniel Santos <daniel.ns.santos@gmail.com>
2021-05-25 00:08:59 -0300

    test: 10

--- server.js
+++ server.js
@@ -11,14 +11,16 @@ const homeRoutes = require('./routes/home.routes');
 mongoose.connect(
   process.env.CONNECTION_STRING,
   { useNewUrlParser: true, useUnifiedTopology: true },
-).then(() => {
-  app.use(express.urlencoded({ extended: true }));
+);
 
-  app.set('views', resolve(__dirname, 'src', 'views'));
-  app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
 
-  app.use(homeRoutes);
+app.set('views', resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+app.use(homeRoutes);
 
 
+mongoose.connection.on('connected', () => {
   app.listen(connectionPort, () => console.log(`successfull listening to port ${connectionPort}`));
 });
