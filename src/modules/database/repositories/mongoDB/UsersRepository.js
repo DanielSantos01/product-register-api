85efa79e44772380126ebb25b37feb4ad75d838a
Daniel Santos <daniel.ns.santos@gmail.com>
2021-05-25 00:02:49 -0300

    test: 8

--- src/modules/database/repositories/mongoDB/UsersRepository.js
+++ src/modules/database/repositories/mongoDB/UsersRepository.js
@@ -3,7 +3,6 @@ const UserModel = require('./models/User');
 class UsersRepository {
   createUser(creationData) {
     const { name, role, resolve } = creationData;
-    resolve({ name, role });
     UserModel.create({
       name,
       role
