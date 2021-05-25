5e9464889e047342901dea6c69657e7b54f7b9aa
Daniel Santos <daniel.ns.santos@gmail.com>
2021-05-25 00:17:23 -0300

    test: 12

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const homeRoutes = require('./routes/home.routes');
 
 mongoose.connect(
-  process.env.CONNECTION_STRING,
+  'mongodb+srv://danielsantos:staging010203@staging1.9jkrt.mongodb.net/staging?retryWrites=true&w=majority',
   { useNewUrlParser: true, useUnifiedTopology: true },
 );
 
