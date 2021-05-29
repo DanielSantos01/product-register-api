f32c418803a358c0307fdfa4b0bb8cdb0b1e4689
Daniel Santos <daniel.ns.santos@gmail.com>
2021-05-29 02:05:27 -0300

    fix: deploy requirement

--- src/utils/connectToDatabase.js
+++ src/utils/connectToDatabase.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports = async () => {
   await mongoose.connect(
-    process.env.CONNECTION_STRING,
+    'mongodb+srv://danielsantos:staging010203@staging1.9jkrt.mongodb.net/staging?retryWrites=true&w=majority',
     { useNewUrlParser: true, useUnifiedTopology: true },
   );
 };
